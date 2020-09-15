98e7dcff078cdb6bcba5e7151f22ad990711b287
David Lynch <kemayo@gmail.com>
2020-09-15 10:23:04 -0500

    Fix the miner so I can update data

--- tools/miner.py
+++ tools/miner.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import json
 import re
 import sys
 
@@ -7,7 +8,7 @@ from bs4 import BeautifulSoup
 
 from fetch import Fetch
 
-WOWHEAD_URL = 'http://www.wowhead.com'
+WOWHEAD_URL = 'https://www.wowhead.com'
 
 
 def soup(html):
@@ -22,27 +23,33 @@ def fetch_data():
     items_soup = soup(items_page)
     item_types = items_soup.find('select', id="filter-facet-type").find_all('option')
     for item_type in item_types:
+        type_url = '%s/items/recipes/type:%s?filter=168;1;0' % (WOWHEAD_URL, item_type['value'])
         # search for items of type which teach a spell
-        print("Searching for", item_type.string)
+        print("Searching for", item_type.string, type_url)
 
-        type_page = fetch('%s/items/recipes/type:%s?filter=168;1;0' % (WOWHEAD_URL, item_type['value']))
-        for data_match in re.finditer(r'var _ = {};\n(_.+)', type_page):
-            for itemid_match in re.finditer(r'_\[(\d+)\]=', data_match.group(1)):
-                itemid = itemid_match.group(1)
+        type_page = fetch(type_url)
+        data_match = re.search(r'var listviewitems = \[\{(.+?)\}\];', type_page, re.DOTALL)
+        # for data_match in re.finditer(r'var listviewitems = \[\{(.+?)\}\];', type_page, re.DOTALL):)
+        if not data_match:
+            print("No items found")
+            continue
 
-                # this is the super-lightweight page used for the "powered by wowhead" tooltips
-                item_page = fetch('%s/item=%s&power' % (WOWHEAD_URL, itemid))
-                if type(item_page) == bytes:
-                    item_page = item_page.decode('utf-8')
+        for itemid_match in re.finditer(r'"id":\s*(\d+)', data_match.group(1)):
+            itemid = itemid_match.group(1)
 
-                name_match = re.search(r"name_enus: '(.+?)',", item_page)
-                # Note: there'll be multiple spellid links. I *think* we can trust the first one to be the "teaches" spellid
-                spellid_match = re.search(r'<a href="/spell=(\d+)"', item_page)
-                if name_match and spellid_match:
-                    print("-", itemid, ":", name_match.group(1), ":", spellid_match.group(1))
-                    data.append((int(itemid), int(spellid_match.group(1)), name_match.group(1).replace("\'", "'")))
-                # else:
-                #     print("Couldn't find spellid", itemid, item_page)
+            # this is the super-lightweight page used for the "powered by wowhead" tooltips
+            item_page = fetch('%s/tooltip/item/%s' % (WOWHEAD_URL, itemid))
+            if type(item_page) == bytes:
+                item_page = item_page.decode('utf-8')
+            item = json.loads(item_page)
+
+            # Note: there'll be multiple spellid links. I *think* we can trust the first one to be the "teaches" spellid
+            spellid_match = re.search(r'<a href="/spell=(\d+)"', item["tooltip"])
+            if spellid_match:
+                print("-", itemid, ":", item["name"], ":", spellid_match.group(1))
+                data.append((int(itemid), int(spellid_match.group(1)), item["name"].replace("\'", "'")))
+            # else:
+            #     print("Couldn't find spellid", itemid, item_page)
     return data
 
 
